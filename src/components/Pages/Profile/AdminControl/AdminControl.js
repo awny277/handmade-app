a8a6042c169b4cbb9d9cdfa6a4131b8a323043db
Abdelrahman-tarek-awny <60525191+awny277@users.noreply.github.com>
2022-06-23 18:42:06 +0200

    update

--- src/components/Pages/Profile/AdminControl/AdminControl.js
+++ src/components/Pages/Profile/AdminControl/AdminControl.js
@@ -1,7 +1,24 @@
 import React from "react";
 import "./AdminControl.css";
+import AdminControlImage from "../../../../image/adminControl.jpg";
 const AdminControl = () => {
-  return <>sadasdsad</>;
+  return (
+    <div className="AdminControl">
+      <div className="infromation">
+        <div className="AdminControlImage">
+          <img src={AdminControlImage} alt="AdminControlImage" />
+        </div>
+        <h3>abdelrahman</h3>
+        <span>user</span>
+      </div>
+      <ul>
+        <li>conrtol panal</li>
+        <li>DashBoard</li>
+        <li>Setting</li>
+        <li>logout</li>
+      </ul>
+    </div>
+  );
 };
 
 export default AdminControl;
