2da6f94ffdc21c275368ec1598514d6e645de3e2
dawoodjunior <zezodawooduwk@gmail.com>
2022-06-23 15:15:41 +0200

    update

--- src/components/Pages/Profile/Profile.js
+++ src/components/Pages/Profile/Profile.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
 const Profile = () => {
-  return <>Hello from profile</>;
+  return <>Hello from profiles</>;
 };
 
 export default Profile;
