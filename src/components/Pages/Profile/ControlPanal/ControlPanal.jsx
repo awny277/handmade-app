6a8718b52999a7bb4f2c07216c7129990c85d250
ahmedfaroUk1oo <ahmedelmahdy33072@gmail.com>
2022-06-24 22:52:26 +0200

    update

--- src/components/Pages/Profile/ControlPanal/ControlPanal.jsx
+++ src/components/Pages/Profile/ControlPanal/ControlPanal.jsx
@@ -1,7 +1,120 @@
 import React from "react";
 import "./ControlPanal.css";
+import { FaShoppingBag ,FaClock } from "react-icons/fa";
 const ControlPanal = () => {
-  return <div>hello from ControlPanal</div>;
+  return (
+    <div className="main-header">
+      <div className="container-process ">
+        <div className="control-panel">
+          <h4>offers</h4>          
+          <div className="control-process">
+
+            <div className="control-01">
+              <img src="https://manager.almadarisp.com/user/img/user.png" alt="" />
+              <p>designer name</p>
+              <span><FaShoppingBag /> specialization</span>
+            </div>
+
+
+            <div className="control-02">
+              <p>project name</p>
+              <span><FaClock /> 0 min ago</span>
+            </div>
+
+
+            <div className="control-03">
+              <p>budget</p>
+              <span className="special-span">EGP830</span>
+            </div>
+
+
+            <div className="control-04">
+              <p>days</p>
+              <span className="special-span">4</span>
+            </div>
+
+
+            <div className="control-05">
+              <button>accept &#10003;</button>
+              <button>cancel &#10005;</button>
+            </div>
+
+
+          </div>
+
+          <div className="control-process">
+
+<div className="control-01">
+  <img src="https://manager.almadarisp.com/user/img/user.png" alt="" />
+  <p>designer name</p>
+  <span><FaShoppingBag /> specialization</span>
+</div>
+
+
+<div className="control-02">
+  <p>project name</p>
+  <span><FaClock /> 0 min ago</span>
+</div>
+
+
+<div className="control-03">
+  <p>budget</p>
+  <span className="special-span">EGP830</span>
+</div>
+
+
+<div className="control-04">
+  <p>days</p>
+  <span className="special-span">4</span>
+</div>
+
+
+<div className="control-05">
+  <button>accept &#10003;</button>
+  <button>cancel &#10005;</button>
+</div>
+
+
+</div>
+
+<div className="control-process">
+
+<div className="control-01">
+  <img src="https://manager.almadarisp.com/user/img/user.png" alt="" />
+  <p>designer name</p>
+  <span><FaShoppingBag /> specialization</span>
+</div>
+
+
+<div className="control-02">
+  <p>project name</p>
+  <span><FaClock /> 0 min ago</span>
+</div>
+
+
+<div className="control-03">
+  <p>budget</p>
+  <span className="special-span">EGP830</span>
+</div>
+
+
+<div className="control-04">
+  <p>days</p>
+  <span className="special-span">4</span>
+</div>
+
+
+<div className="control-05">
+  <button>accept &#10003;</button>
+  <button>cancel &#10005;</button>
+</div>
+
+
+</div>
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default ControlPanal;
