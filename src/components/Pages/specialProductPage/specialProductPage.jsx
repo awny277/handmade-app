f027365ffacf06c8a7e6e4f7aeab8c8a81d50f7a
Abdelrahman-tarek-awny <60525191+awny277@users.noreply.github.com>
2022-07-06 22:25:46 +0200

    update

--- src/components/Pages/specialProductPage/specialProductPage.jsx
+++ src/components/Pages/specialProductPage/specialProductPage.jsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
 import { Col, Container, Row, FloatingLabel, Form } from "react-bootstrap";
 import HeaderImage from "../../../image/slide2.jpg";
 import { useNavigate } from "react-router-dom";
@@ -9,6 +11,14 @@ import "./specialProductPage.css";
 // IoIosAdd
 const SpecialProductPage = (props) => {
   const navigate = useNavigate();
+  const [result, setResult] = useState([]);
+  useEffect(() => {
+    // /special_orders"
+    axios.get("http://127.0.0.1:5000//special_orders").then((res) => {
+      const Final = res.data;
+      setResult(Final);
+    });
+  }, []);
   return (
     <div>
       <div className="Header-Container">
@@ -41,6 +51,39 @@ const SpecialProductPage = (props) => {
                     <span>DelivarDays: {ele.delivarDays}</span>
                     <span>{moment(ele.time).fromNow()}</span>
                     <p>Description: {ele.projectDetails}</p>
+                    <button
+                      className="btn"
+                      onClick={() => {
+                        navigate(`/specialProductDetailsPage/${ele.id}`);
+                      }}
+                    >
+                      add your offer <IoIosAdd />
+                    </button>
+                  </div>
+                </Col>
+              );
+            })}
+          </Row>
+          <Row>
+            {result.map((ele, idx) => {
+              return (
+                <Col md={6} key={idx}>
+                  <div className="newProject-page-Contet m-2" key={idx}>
+                    <h3>{ele.title}</h3>
+                    <div className="CategTypetype">
+                      <h4>
+                        type: <span>{ele.category}</span>
+                      </h4>
+                      {ele.category !== "" && (
+                        <h5>
+                          subtype: <span>{ele.sub_category}</span>
+                        </h5>
+                      )}
+                    </div>
+                    <span>budget: {ele.expected_budget}</span>
+                    <span>DelivarDays: {ele.est_delivery_time}</span>
+                    {/* <span>{moment(ele.time).fromNow()}</span> */}
+                    <p>Description: {ele.description}</p>
                     <button
                       className="btn"
                       onClick={() => {
